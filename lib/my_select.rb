0068cc7f1c72eac129090d15b25f33e60b98be97
github username <github email address>
2019-05-16 19:57:35 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+	i = 0
+	new_selection = []
+	while i < collection.length
+		new_selection.push(collection[i]) if yield collection[i]
+		i+=1
+	end
+	new_selection
 end
+
+# new_array = [1,2,3,4,5]
+# p my_select(new_array) {|n| n.even?} 
